c0ed0b27ecc0b4360eb2b5c0f8b4fc2c63c87d1a
Mike Foerster <mfoerster@mysticprogrammer.com>
2019-07-11 19:42:25 -0400

    Errors fix
    I fixed all the errors that I could find, and put a comment beside where the error was and said what I fixed.

--- index.php
+++ index.php
@@ -15,67 +15,91 @@ $people = array(
 	array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
 );
 
-$person = $_POST['person'];
+$person = $_GET['person']; ## Switched from a $_POST to a $_GET
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Interview test</title>
-	<style>
-		body {font: normal 14px/1.5 sans-serif;}
-	</style>
+    <meta charset="UTF-8" />
+    <title>Interview test</title>
+    <style>
+        body {
+            font: normal 14px/1.5 sans-serif;
+        }
+    </style>
 </head>
 <body>
 
-	<h1><?=Interview::$title;?></h1>
+    <h1>
+        <?php $title = new Interview; #declared the interview class ?>
+        <?=$title->title;?>
+    </h1>
 
-	<?php
+    <?php
 	// Print 10 times
-	for ($i=10; $i<0; $i++) {
-		echo '<p>'+$lipsum+'</p>';
+	for ($i=0; $i<10; $i++) { #switch the start to be 0 and the end to be 10
+		echo '<p>' . $lipsum . '</p>';  #changed pluses to dots
 	}
-	?>
+    ?>
 
 
-	<hr>
+    <hr />
 
 
-	<form method="get" action="<?=$_SERVER['REQUEST_URI'];?>">
-		<p><label for="firstName">First name</label> <input type="text" name="person[first_name]" id="firstName"></p>
-		<p><label for="lastName">Last name</label> <input type="text" name="person[last_name]" id="lastName"></p>
-		<p><label for="email">Email</label> <input type="text" name="person[email]" id="email"></p>
-		<p><input type="submit" value="Submit" /></p>
-	</form>
+    <form method="get" action="<?=$_SERVER['REQUEST_URI'];?>">
+        <p>
+            <label for="firstName">First name</label>
+            <input type="text" name="person[first_name]" id="firstName"/>
+        </p>
+        <p>
+            <label for="lastName">Last name</label>
+            <input type="text" name="person[last_name]" id="lastName" />
+        </p>
+        <p>
+            <label for="email">Email</label>
+            <input type="text" name="person[email]" id="email" />
+        </p>
+        <p>
+            <input type="submit" value="Submit" />
+        </p>
+    </form>
 
-	<?php if ($person): ?>
-		<p><strong>Person</strong> <?=$person['first_name'];?>, <?=$person['last_name'];?>, <?=$person['email'];?></p>
-	<?php endif; ?>
+    <?php if ($person): ?>
+    <p>
+        <strong>Person</strong><?=$person['first_name'];?>, <?=$person['last_name'];?>, <?=$person['email'];?>
+    </p>
+    <?php endif; ?>
 
 
-	<hr>
+    <hr />
 
 
-	<table>
-		<thead>
-			<tr>
-				<th>First name</th>
-				<th>Last name</th>
-				<th>Email</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($people as $person): ?>
-				<tr>
-					<td><?=$person->first_name;?></td>
-					<td><?=$person->last_name;?></td>
-					<td><?=$person->email;?></td>
-				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
+    <table>
+        <thead>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($people as $person): ?>
+            <tr>
+                <td>
+                    <?=$person['first_name']; #changed to a proper array ?>
+                </td>
+                <td>
+                    <?=$person['last_name']; #changed to a proper array ?>
+                </td>
+                <td>
+                    <?=$person['email']; #changed to a proper array ?>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 
 </body>
 </html>
